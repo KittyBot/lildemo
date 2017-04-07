7513a9bdefff66b60ff3604391cc7f7d1833ef42
Christina Keelan <christinakeelan@gmail.com>
2017-04-07 18:51:08 -0500

    some more stuff just happened

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
+// var http = require('http').Server(app);
+// var router = express.Router();
+//
+// router.get('/', function(req, res){
+//   res.send('<h1>Hello world</h1>');
+// });
+//
+// app.use(bodyParser.json());
+// app.use('/',router);
+//
+// http.listen(3000, function(){
+//   console.log('listening on port 3000');
+// });
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
+// var http = require('http').Server(app);
+//
+// /**
+//   Adding the controllers.
+// */
+// app.use(bodyParser.json());
+// app.use(require('./controllers'));
+//
+// http.listen(3000, function(){
+//   console.log('listening on port 3000');
+// });
+//
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
-var router = express.Router();
+var db = require('./models/db');
 
-router.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
-
+/**
+  Adding the controllers.
+*/
+var dbModel = new db();
+/**
+  Setting up the database and creating table.
+*/
+dbModel.setupDb();
 app.use(bodyParser.json());
-app.use('/',router);
+app.use(require('./controllers'));
 
 http.listen(3000, function(){
   console.log('listening on port 3000');
